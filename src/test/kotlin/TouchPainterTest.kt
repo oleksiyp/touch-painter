2631ac79bc76cca7b9a9f3a4421529d8bbfbee14
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2017-12-28 22:36:37 +0100

    kotlinFrontendPlugin

--- src/test/kotlin/TouchPainterTest.kt
+++ src/test/kotlin/TouchPainterTest.kt
@@ -2,6 +2,7 @@ import kotlinx.html.dom.create
 import kotlinx.html.js.body
 import kotlin.browser.document
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class TouchPainterTest {
     @Test
@@ -10,6 +11,7 @@ class TouchPainterTest {
 
         val html = document.create.body()
         painter.onStart(html)
-        println(html.innerHTML)
+
+        assertEquals("<section id=\"paintArea\"><canvas id=\"canvas\"></canvas></section><section id=\"controlArea\"><button id=\"red\"></button><button id=\"green\"></button><button id=\"blue\"></button><a id=\"save\" href=\"#\" download=\"image.png\"><button id=\"previewContainer\"></button></a></section>", html.innerHTML)
     }
 }
